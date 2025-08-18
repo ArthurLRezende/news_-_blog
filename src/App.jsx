d0083a42f8b0692739066f0da9ea6b83650d4217
Arthur Rezende <arthurrezende165@gmail.com>
2025-08-18 20:11:16 -0300

    primeiro-commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import News from "./Components/News";
+
+const App = () => {
+  return <div className="container">
+    <div className="news-blog-app"><News /></div>
+  </div>
+}
+
+export default App;
