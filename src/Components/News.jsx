a8c8536d1df8c985bd069d7a3a9cb9f071666cfe
Arthur Rezende <arthurrezende165@gmail.com>
2025-08-19 22:55:15 -0300

    api_Gnews

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,27 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Weather from "./Weather";
 import Calendar from "./Calendar";
 import "./News.css";
 import profileimg from "../assets/images/user.jpg"
-import techImg from "../assets/images/tech.jpg"
-import sportsImg from "../assets/images/sports.jpg"
-import scienceImg from "../assets/images/science.jpg"
-import worldImg from "../assets/images/world.jpg"
-import healthImg from "../assets/images/health.jpg"
-import nationImg from "../assets/images/nation.jpg"
+import noImage from "../assets/images/no-img.png"
+import axios from "axios"
 
 
 const News = () => {
-    
+
     const api_key = import.meta.env.VITE_apiKey;
 
-    
+    const categories = ["general", "world", "business", "technology", "entertainment", "sports", "science", "health", "nation"]
+
+    const [headline, setHeadline] = useState(null)
+    const [news, setNews] = useState([])
+    const [selectedCategory, setSelectedCategory] = useState("general")
+    const [searchInput, setSearchInput] = useState('')
+    const [searchQuery, setSearchQuery] = useState('')
+
+    useEffect(() => {
+        const fetchNews = async () => {
+            let url = 'https://gnews.io/api/v4/top-headlines?category=' + selectedCategory + '&lang=en&apikey=' + api_key
+
+            if (searchQuery) {
+                url = `https://gnews.io/api/v4/search?q=${searchQuery}&lang=en&max=5&apikey=${api_key}`
+            }
+
+            const response = await axios.get(url)
+            console.log(url)
+            const fetchedNews = response.data.articles
+
+            setHeadline(fetchedNews[0])
+            console.log(fetchedNews[0])
+            setNews(fetchedNews.slice(1, 7))
+
+        }
+
+        fetchNews()
+    }, [selectedCategory, searchQuery])
+
+    const handleCategoryClick = (e, category) => {
+        e.preventDefault()
+        setSearchQuery('')
+        setSelectedCategory(category)
+    }
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        setSearchQuery(searchInput)
+        setSearchInput('')
+    }
+
     return <div className="news">
         <header className="news-header">
             <h1 className="logo">News & Blogs</h1>
             <div className="search-bar">
-                <form>
-                    <input type="text" placeholder="Search News ..." />
+                <form onSubmit={handleSearch}>
+                    <input type="text" value={searchInput}
+                        onChange={(e) => {
+                            setSearchInput(e.target.value)
+                        }} placeholder="Search News ..." />
                     <button type="submit"><i className="fa-solid fa-magnifying-glass"></i></button>
                 </form>
             </div>
@@ -35,49 +74,31 @@ const News = () => {
                 <div className="categories">
                     <h1 className="nav-header">Categories</h1>
                     <div className="nav-links">
-                        <a href="" className="nav-link">General</a>
-                        <a href="" className="nav-link">World</a>
-                        <a href="" className="nav-link">Business</a>
-                        <a href="" className="nav-link">Technology</a>
-                        <a href="" className="nav-link">Entertainment</a>
-                        <a href="" className="nav-link">Sports</a>
-                        <a href="" className="nav-link">Science</a>
-                        <a href="" className="nav-link">Health</a>
-                        <a href="" className="nav-link">Nation</a>
+                        {categories.map((category) => (
+                            <a href="#" key={category} onClick={(e) => {
+                                handleCategoryClick(e, category)
+                            }} className="nav-link">{category}</a>
+                        ))}
                         <a href="" className="nav-link">Bookmarks <i className="fa-regular fa-bookmark"></i></a>
                     </div>
                 </div>
             </div>
             <div className="news-section">
-                <div className="headline">
-                    <img src={techImg} alt="Headline Image" />
-                    <h2 className="headline-title">Lorem ipsum aaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa... <i className="fa-regular fa-bookmark bookmark"></i></h2>
-                </div>
+                {headline && (
+                    <div className="headline">
+                        <img src={headline.image} alt={headline.title} />
+                        <h2 className="headline-title">{headline.title}<i className="fa-regular fa-bookmark bookmark"></i></h2>
+                    </div>
+                )}
                 <div className="news-grid">
-                    <div className="news-grid-item">
-                        <img src={techImg} alt="News Image" />
-                        <h3>lorem ipsum ...<i className="fa-regular fa-bookmark bookmark"></i> </h3>
-                    </div>
-                      <div className="news-grid-item">
-                        <img src={sportsImg} alt="News Image" />
-                        <h3>lorem ipsum ...<i className="fa-regular fa-bookmark bookmark"></i> </h3>
-                    </div>
-                      <div className="news-grid-item">
-                        <img src={scienceImg} alt="News Image" />
-                        <h3>lorem ipsum ...<i className="fa-regular fa-bookmark bookmark"></i> </h3>
-                    </div>
-                      <div className="news-grid-item">
-                        <img src={worldImg} alt="News Image" />
-                        <h3>lorem ipsum ...<i className="fa-regular fa-bookmark bookmark"></i> </h3>
-                    </div>
-                      <div className="news-grid-item">
-                        <img src={healthImg} alt="News Image" />
-                        <h3>lorem ipsum ...<i className="fa-regular fa-bookmark bookmark"></i> </h3>
-                    </div>
-                      <div className="news-grid-item">
-                        <img src={nationImg} alt="News Image" />
-                        <h3>lorem ipsum ...<i className="fa-regular fa-bookmark bookmark"></i> </h3>
-                    </div>
+                    {news && (news.map((article, index) => (
+                        <div key={index} className="news-grid-item">
+                            {article.image ? <img src={article.image} alt={article.title} /> :
+                                <img src={noImage} alt={article.title} />}
+
+                            <h3>{article.title}<i className="fa-regular fa-bookmark bookmark"></i> </h3>
+                        </div>
+                    )))}
                 </div>
             </div>
             <div className="my-blogs">My blogs</div>
