f6d3eb175b693eb39980c2651959d532197bf1c7
Arthur Rezende <arthurrezende165@gmail.com>
2025-10-08 17:40:09 -0300

    blog

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -9,8 +9,23 @@ const Blogs = () => {
                 <img src={image} alt="" />
             </div>
             <div className="blogs-right">
+                {/*<button className="post-btn">Create Post</button> */}
+                <div className="blogs-right-form">
+                    <h1>New Post</h1>
+                    <form>
+                        <div className="img-upload">
+                            <label htmlFor="file-upload" className="file-upload">
+                                <i class='bxr  bx-file-plus'  ></i>  Upload Image
+                            </label>
+                        </div>
+                        <input type="text" 
+                        placeholder="Add Title (Max 60 Characters)" 
+                        className="title-input"/>
+                        <textarea className="text-input" placeholder="Add Text"></textarea>
+                        <button type="submit" className="submit-btn">Submit button</button>
+                    </form>
+                </div>
                 <button className="blogs-close-btn">Back<i className="bx bx-chevron-right"></i></button>
-                <button className="post-btn">Create Post</button>
             </div>
         </div>
     )
